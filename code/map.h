8539b7ea2d30a1cc6597f442b29703d9fcbeeee8
Camille <CamilleeeeDing@gmail.com>
2022-05-07 20:21:30 +0800

    update code

---
+++ code/map.h
@@ -0,0 +1,14 @@
+#ifndef __MAP_H__
+#define __MAP_H__
+
+class Map{
+private:
+    static const int nrows = 20; //number of rows
+    static const int ncols = 20; //number of columns
+    
+public:
+    Map(){}
+    ~Map(){}
+};
+
+#endif /* map_h */
