c0dd9d52a5369fbd62788c3487acd0e984858f9f
Camille <CamilleeeeDing@gmail.com>
2022-05-07 22:57:55 +0800

    update

--- code/player.h
+++ code/player.h
@@ -1,8 +1,16 @@
 #ifndef __PLAYER_H__
 #define __PLAYER_H__
 
-class player{
+#include <iostream>
+#include <cstdlib>
+#include "map.h"
+using namespace std;
 
+class player{
+private:
+    Pos character;
+    int score;
+public:
 };
 
 #endif
