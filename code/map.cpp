6989cd0f7edf07d20d5234e2a6739f7513e597ce
Springcty <2415225086@qq.com>
2022-05-07 21:49:51 +0800

    update

--- code/map.cpp
+++ code/map.cpp
@@ -8,23 +8,23 @@ int dir[4][2] = {
     {-1,0}, // up
     {0,1}   // right
 };
+
 bool check(int x, int y){
     return x<=ROWNUM && x>=0 && y<=ROWNUM && y>=0;
 }
 
-bool dfs(bool map[][ROWNUM], Pos start, Pos dest){
+bool dfs(int map[][ROWNUM], Pos start, Pos dest){
     if(start == dest) return true;
 
     for(int i=0; i<4; i++){ // search in four directions
         int a = start.x + dir[i][0];
         int b = start.y + dir[i][1];
-        if(check(a,b) && !map[a][b]){
-            map[a][b] = 1;
-            if(dfs(map, Pos(a,b), dest)) return true;
+        if(check(a,b) && map[a][b]!=0){
             map[a][b] = 0;
+            if(dfs(map, Pos(a,b), dest)) return true;
+            map[a][b] = 1;
         }
     }
-    // s.pop(); // if all of the four directions are walls,
     return false;
 }
 
